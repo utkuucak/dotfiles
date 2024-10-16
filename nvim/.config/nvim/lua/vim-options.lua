0ff5e4757242900207831f6877245337befa4076
Utku Uçak <utku.ucak@hhi.fraunhofer.de>
2024-10-16 19:51:16 +0200

    add: vim-tmux-navigator

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -8,6 +8,12 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set expandtab")
 vim.g.mapleader = " "
 
+-- Navigate vim panes
+vim.keymap.set('n', 'c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', 'c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', 'c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', 'c-l>', ':wincmd l<CR>')
+
 -- line numbering
 vim.cmd("set number")
 vim.cmd("set relativenumber")
