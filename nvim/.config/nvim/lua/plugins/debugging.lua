8ab768b7aad11eb786d2fa4d8c9103378eafcb0f
Utku Uçak <utku.ucak@hhi.fraunhofer.de>
2024-10-17 00:07:15 +0200

    add: c debugging in neovim

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -1,53 +1,78 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-      "nvim-neotest/nvim-nio",
-    },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
 
-      require("dapui").setup()
+			require("dapui").setup()
 
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 
-      dap.configurations.python = {
-        {
-          type = "python",
-          request = "launch",
-          name = "Launch this file",
-          program = "${file}",
-        },
-      }
+			dap.configurations.python = {
+				{
+					type = "python",
+					request = "launch",
+					name = "Launch this file",
+					program = "${file}",
+				},
+			}
 
-      vim.keymap.set("n", "<Leader>b", function()
-        dap.toggle_breakpoint()
-      end)
-      vim.keymap.set("n", "<F5>", function()
-        dap.continue()
-      end)
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      require("dap-python").setup()
-    end,
-  },
+			dap.configurations.c = {
+				{
+					name = "Launch file",
+					type = "cppdbg",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopAtEntry = true,
+				},
+			}
+
+			vim.keymap.set("n", "<Leader>b", function()
+				dap.toggle_breakpoint()
+			end)
+			vim.keymap.set("n", "<F5>", function()
+				dap.continue()
+			end)
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = {
+					"python",
+					"cppdbg",
+				},
+				handlers = {},
+			})
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			require("dap-python").setup()
+		end,
+	},
 }
