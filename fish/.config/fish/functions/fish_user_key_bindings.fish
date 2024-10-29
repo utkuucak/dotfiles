046b0bcf598c2849bd1b788903ea4b760bfb7502
Utku Uçak <utku.ucak@hhi.fraunhofer.de>
2024-10-29 13:23:14 +0100

    update c-p and c-n as command history in fish vim insert mode

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,11 +1,7 @@
-if status is-interactive
-  fish_vi_key_bindings
-
+function fish_user_key_bindings
   # Ctrl + n for next command
   bind -M insert \cp "commandline -f history-search-backward"
 
   # Ctrl + p for previous command
   bind -M insert \cn "commandline -f history-search-forward"  
 end
-
-
