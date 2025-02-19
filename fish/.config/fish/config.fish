a0dd3a5307dceb5011eba5ad2ea05ee7d862a69d
Utku Ucak <utku.ucak@hhi.fraunhofer.de>
2025-02-19 16:30:39 +0000

    fish: grc abbr for gnuradio

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,9 @@ if status is-interactive
 
   # Ctrl + p for previous command
   bind -M insert \cn "commandline -f history-search-forward"  
+
+  abbr --add grc env PYTHONPATH=/usr/lib/python3/dist-packages:/usr/lib/python3/site-packages LD_LIBRARY_PATH=/usr/lib gnuradio-companion %F
+
 end
 
 
